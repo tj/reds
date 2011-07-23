0ce62eb4257174864b9294c6cc4ebac0fe70f239
Tj Holowaychuk <tj@vision-media.ca>
2011-07-22 19:46:32 -0700

    metaphone map / array

--- test.js
+++ test.js
@@ -4,5 +4,23 @@
  */
 
 var should = require('should')
-  , reds = require('./');
+  , redis = require('redis')
+  , reds = require('./')
+  , db = redis.createClient();
 
+var strs = [];
+strs.push('Tobi wants four dollars');
+strs.push('Tobi only wants $4');
+strs.push('Loki is really fat');
+strs.push('Loki, Jane, and Tobi are ferrets');
+
+strs.forEach(function(str){
+  var map = reds.metaphoneMap(str);
+  for (var word in map) {
+    db.sadd('word:' + map[word], word);
+  }
+});
+
+var query = 'Tobi'
+  , arr = reds.metaphoneArray(query);
+console.log(arr);
