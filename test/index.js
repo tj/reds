c3dbb119f3b37b4acf8ca62539704335045d66b0
Tj Holowaychuk <tj@vision-media.ca>
2011-07-25 20:03:06 -0700

    added more tests

--- test/index.js
+++ test/index.js
@@ -32,3 +32,17 @@ reds
   .metaphoneKeys(['foo', 'bar', 'baz'])
   .should.eql(['word:F', 'word:BR', 'word:BS']);
 
+reds
+  .add('Tobi wants 4 dollars', 0)
+  .add('Tobi wants $4', 1)
+  .add('Loki is a ferret', 2)
+  .add('Tobi is also a ferret', 3)
+  .add('Jane is a bitchy ferret', 4)
+  .add('Tobi is employed by LearnBoost', 5);
+
+reds
+  .search('Tobi', function(err, ids){
+    if (err) throw err;
+    ids.should.eql([0, 1, 3, 5]);
+    process.exit();
+  });
