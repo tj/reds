712451eabdd2c64fa2ed3d9744633a7a827175f2
Tj Holowaychuk <tj@vision-media.ca>
2011-07-25 20:11:38 -0700

    more tests

--- test/index.js
+++ test/index.js
@@ -34,21 +34,57 @@ reds
 
 reds
   .add('Tobi wants 4 dollars', 0)
-  .add('Tobi wants $4', 1)
   .add('Loki is a ferret', 2)
   .add('Tobi is also a ferret', 3)
   .add('Jane is a bitchy ferret', 4)
   .add('Tobi is employed by LearnBoost', 5);
 
+var pending = 0;
+
+++pending;
 reds
   .search('Tobi', function(err, ids){
     if (err) throw err;
-    ids.should.eql([0, 1, 3, 5]);
+    ids.should.eql([0, 3, 5]);
+    --pending || process.exit();
   });
 
+++pending;
+reds
+  .search('tobi', function(err, ids){
+    if (err) throw err;
+    ids.should.eql([0, 3, 5]);
+    --pending || process.exit();
+  });
+
+++pending;
+reds
+  .search('bitchy', function(err, ids){
+    if (err) throw err;
+    ids.should.eql([4]);
+    --pending || process.exit();
+  });
+
+++pending;
+reds
+  .search('bitchy jane', function(err, ids){
+    if (err) throw err;
+    ids.should.eql([4]);
+    --pending || process.exit();
+  });
+
+++pending;
+reds
+  .search('loki and jane', function(err, ids){
+    if (err) throw err;
+    ids.should.eql([2, 4]);
+    --pending || process.exit();
+  }, 'or');
+
+++pending;
 reds
   .search('is a', function(err, ids){
     if (err) throw err;
     ids.should.eql([]);
-    process.exit();
+    --pending || process.exit();
   });
