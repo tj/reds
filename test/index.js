813765dab6f54d6916418a7ed7a0a9010f260312
Tj Holowaychuk <tj@vision-media.ca>
2011-07-27 16:02:36 -0700

    fixed tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,9 @@
  */
 
 var reds = require('../')
-  , should = require('should');
+  , should = require('should')
+  , redis = require('redis')
+  , db = redis.createClient();
 
 reds.version.should.match(/^\d+\.\d+\.\d+$/);
 
@@ -32,83 +34,87 @@ reds
   .metaphoneKeys(['foo', 'bar', 'baz'])
   .should.eql(['word:F', 'word:BR', 'word:BS']);
 
-reds
-  .add('Tobi wants 4 dollars', 0)
-  .add('Loki is a ferret', 2)
-  .add('Tobi is also a ferret', 3)
-  .add('Jane is a bitchy ferret', 4)
-  .add('Tobi is employed by LearnBoost', 5);
+db.flushdb(function(){
+  reds
+    .add('Tobi wants 4 dollars', 0)
+    .add('Loki is a ferret', 2)
+    .add('Tobi is also a ferret', 3)
+    .add('Jane is a bitchy ferret', 4)
+    .add('Tobi is employed by LearnBoost', 5, test);
+});
 
-var pending = 0;
+function test() {
+  var pending = 0;
 
-++pending;
-reds
-  .search('Tobi', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([0, 3, 5]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('Tobi', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([0, 3, 5]);
+      --pending || process.exit();
+    });
 
-++pending;
-reds
-  .search('tobi', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([0, 3, 5]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('tobi', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([0, 3, 5]);
+      --pending || process.exit();
+    });
 
-++pending;
-reds
-  .search('bitchy', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([4]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('bitchy', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([4]);
+      --pending || process.exit();
+    });
 
-++pending;
-reds
-  .search('bitchy jane', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([4]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('bitchy jane', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([4]);
+      --pending || process.exit();
+    });
 
-++pending;
-reds
-  .search('loki and jane', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([2, 4]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('loki and jane', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([2, 4]);
+      --pending || process.exit();
+    });
 
-++pending;
-reds
-  .search('loki and jane', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([2, 4]);
-    --pending || process.exit();
-  }, 'or');
+  ++pending;
+  reds
+    .search('loki and jane', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([2, 4]);
+      --pending || process.exit();
+    }, 'or');
 
-++pending;
-reds
-  .search('loki and jane', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([]);
-    --pending || process.exit();
-  }, 'and');
+  ++pending;
+  reds
+    .search('loki and jane', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([]);
+      --pending || process.exit();
+    }, 'and');
 
-++pending;
-reds
-  .search('jane ferret', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([4]);
-    --pending || process.exit();
-  }, 'and');
+  ++pending;
+  reds
+    .search('jane ferret', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([4]);
+      --pending || process.exit();
+    }, 'and');
 
-++pending;
-reds
-  .search('is a', function(err, ids){
-    if (err) throw err;
-    ids.should.eql([]);
-    --pending || process.exit();
-  });
+  ++pending;
+  reds
+    .search('is a', function(err, ids){
+      if (err) throw err;
+      ids.should.eql([]);
+      --pending || process.exit();
+    });
+}
