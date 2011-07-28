8aa06dd77872d303eb3d228b41503d924c38c3fe
Tj Holowaychuk <tj@vision-media.ca>
2011-07-28 08:57:48 -0700

    fixed simple example

--- examples/simple.js
+++ examples/simple.js
@@ -3,7 +3,8 @@
  * Module dependencies.
  */
 
-var reds = require('../');
+var reds = require('../')
+  , search = reds.createSearch('pets');
 
 // $ node examples/simple Tobi
 // $ node examples/simple tobi
@@ -25,12 +26,12 @@ if (!query) throw new Error('query required');
 // index them
 
 strs.forEach(function(str, i){
-  reds.add(str, i);
+  search.index(str, i);
 });
 
 // query
 
-reds.search(query, function(err, ids){
+search.query(query, function(err, ids){
   if (err) throw err;
   var res = ids.map(function(i){ return strs[i]; });
   console.log();
