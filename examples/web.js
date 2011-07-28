e52627274ebc26cd53a6ce4481c94d0769827eed
Tj Holowaychuk <tj@vision-media.ca>
2011-07-27 19:15:32 -0700

    coloring

--- examples/web.js
+++ examples/web.js
@@ -12,23 +12,29 @@ var reds = require('../')
 var urls = [];
 
 urls.push('http://learnboost.com');
+urls.push('http://manning.com');
 
 // index
 
 var pending = urls.length;
 
 urls.forEach(function(url, i){
-  console.log('  fetching %s', url);
+  function log(msg) {
+    console.log('  \033[90m%s \033[36m%s\033[0m', msg, url);
+  }
+
+  log('fetching');
   agent.get(url, function(err, res, body){
     if (err) throw err;
     // strip html tags
-    console.log('  stripping tags %s', url);
+    log('stripping tags');
     body = striptags(body);
 
-    console.log('  indexing %s', url);
+    // index
+    log('indexing');
     reds.add(body, i, function(err){
       if (err) throw err;
-      console.log('  completed %s', url);
+      log('completed');
     });
   });
 });
@@ -36,6 +42,5 @@ urls.forEach(function(url, i){
 // lame, dont use me
 
 function striptags(html) {
-  return String(html)
-    .replace(/<\/?([^>]+)>/g, '');
+  return String(html).replace(/<\/?([^>]+)>/g, '');
 }
