9c1e6677f53f44e949abebe7c536c736421790b8
Tj Holowaychuk <tj@vision-media.ca>
2011-07-27 19:31:06 -0700

    web-search example

---
+++ examples/web-search.js
@@ -0,0 +1,30 @@
+
+/**
+ * Module dependencies.
+ */
+
+var reds = require('../')
+  , fs = require('fs')
+  , urls = fs.readFileSync(__dirname + '/urls', 'utf8').split('\n');
+
+// First run:
+//   $ node examples/web-index
+// Then query:
+//   $ node examples/web-search whatever query here
+
+var query = process.argv.slice(2).join(' ');
+if (!query) throw new Error('query required');
+
+// query
+
+reds.search(query, function(err, ids){
+  if (err) throw err;
+  var res = ids.map(function(i){ return urls[i]; });
+  console.log();
+  console.log('  Search results for "%s"', query);
+  res.forEach(function(str){
+    console.log('    - %s', str);
+  });
+  console.log();
+  process.exit();
+});
